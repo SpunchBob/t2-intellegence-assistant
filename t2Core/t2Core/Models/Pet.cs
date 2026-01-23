5d2538912b03225a6fc8e8553f53262a3aa260a5
sqtwix <ivan20140767@gmail.com>
2026-01-23 13:20:28 +0300

    feat: added auto start migrations

---
+++ t2Core/t2Core/Models/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace t2Core.Models
+{
+    public class Pet
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }  // FK к User
+        public string? Type { get; set; }
+        public string? Location { get; set; }
+        public string? Crown { get; set; }
+
+        public User User { get; set; } = null!;
+    }
+}
