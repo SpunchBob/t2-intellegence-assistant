290e3b41240dde01d288288f6d60b88fad8c12ce
sqtwix <ivan20140767@gmail.com>
2026-01-23 14:38:14 +0300

    feat: PetDTO was added

---
+++ t2Core/t2Core/DTOs/PetDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace t2Core.DTOs
+{
+    public class PetDTO
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } = string.Empty;  // Тип питомца
+        public string? Location { get; set; }  // Опционально, null если нет
+        public string? Crown { get; set; }  // Опционально, null если нет
+    }
+}
